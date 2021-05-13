00810878361d9c1196e54f4b704990f6fc17dff7
minchjung <78589751+minchjung@users.noreply.github.com>
2021-05-14 00:03:42 +0900

    1101Geometry[V3]3053__cpp

---
+++ DS3_1011Geometry03/1101Geometry[V3]3053.cpp
@@ -0,0 +1,10 @@
+// 1101<Geometry>[V3]3053_cpp
+#include<bits/stdc++.h>
+using namespace std;
+#define pi 3.14159265358979323846
+int main(){
+    int r ; 
+    cin >> r ; 
+    printf("%.6f\n",pi*r*r);
+    printf("%.6f\n",2.0*r*r);
+}
